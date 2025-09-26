4e6fb0d21a492efc15fa5448339e3264eb1d2cec
liaperc <liaperc@nuevaschool.org>
2025-09-25 19:20:09 -0700

    made the button now retrieve the csv and out put it after brief modification

--- Extension/scripts/background.js
+++ Extension/scripts/background.js
@@ -1,10 +1,37 @@
-//TODO : download libraries instead of importing
-(() => {
-    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-        if (changeInfo.status === 'complete' && tab.url && tab.url.includes('instructure.com') && tab.url.includes('gradebook')) {
-            chrome.tabs.sendMessage(tabId, {type: 'GRADEBOOK_LOADED'});
-        }
-    });
-})();
+// Background script for Canvas Rubric Extension
 
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url && tab.url.includes('instructure.com') && tab.url.includes('gradebook')) {
+        chrome.tabs.sendMessage(tabId, {type: 'GRADEBOOK_LOADED'});
+    }
+});
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'fetchCSV') {
+        console.log("Background script: Fetching CSV from", request.url);
+        
+        fetch(request.url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('HTTP ' + response.status + ': ' + response.statusText);
+                }
+                return response.text();
+            })
+            .then(data => {
+                console.log("Background script: CSV fetched successfully, length:", data.length);
+                sendResponse({
+                    success: true,
+                    data: data
+                });
+            })
+            .catch(error => {
+                console.error("Background script: Error fetching CSV:", error);
+                sendResponse({
+                    success: false,
+                    error: error.message
+                });
+            });
+        
+        return true;
+    }
+});
